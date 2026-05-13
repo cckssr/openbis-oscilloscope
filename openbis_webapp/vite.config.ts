796d17be72e50876dab4e2b79f0b6308d9819321
cckssr <cedric.kessler@me.com>
2026-05-13 12:36:07 +0200

    feat: set base path for Vite configuration to "/oscilloscope/"

--- openbis_webapp/vite.config.ts
+++ openbis_webapp/vite.config.ts
@@ -16,6 +16,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: "/oscilloscope/",
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
